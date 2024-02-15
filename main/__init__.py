2d6a7a8fa8632cc72ec0513d803b596cd55d335f
denverki <101944608+denverki@users.noreply.github.com>
2024-02-15 10:01:19 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,17 +12,14 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
-
-bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
-
-userbot = Client("saverestricted", session_string=SESSION, api_hash=API_HASH, api_id=API_ID) 
-
+API_ID = 18910664
+API_HASH = "3884d9cc5d521bb478f5bc8b8ccbec33"
+BOT_TOKEN = "6351561304:AAHWhXB9lHfw-R6B4tSxyaw-EE5PaTBoxsw"
+SESSION = "BQEgjcgAMuLPOwebYJRR4TkXYaiY8t8uQQvn2jFxbqR-TfD03GRuYb8P3XYPoF06Tkp50JQQgOu9xW2up_DELs-po39NYHvNyjjJ_d8W_pqSgV9EksBtWrI8aSBZZfJoKdBLOxxwBxiEwCHdzS2hhMnuRC9N969u1tfAon660CiquJkY5fHjlyJIWqZP0PfFe9kZMbJLFkuwYLFdA2gsHIcV2hEprYvcVXcSZd88BZtnZfM6Wns40vXpcyUGQh2oCcvG7SDG0WBwozjn0p9Tuk4nZ5DK0fHdaxO6g8ndwdB6O_qrd9ufoxqhX0V8rKxvIpRMCEMqNw09jH6ZS4Dd9KYr7mPBMgAAAAAksN1QAA"
+FORCESUB = "saverrre"
+AUTH = 615570768
+bot = "@Seex_mexfexbot"
+userbot = "@Seex_mexfexbot"
 try:
     userbot.start()
 except BaseException:
